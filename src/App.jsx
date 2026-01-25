9f220814315035d30992f88b937c0764a47de2f2
Thunderbeast5 <163726116+Thunderbeast5@users.noreply.github.com>
2026-01-25 09:17:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="App bg-red-400 min-h-screen">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
