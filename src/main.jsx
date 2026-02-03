eecfa5ce551649a240c86de284b20309e4278359
Thunderbeast5 <163726116+Thunderbeast5@users.noreply.github.com>
2026-02-03 21:30:22 +0530

    buffer issue fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import { Buffer } from 'buffer'
+import { Buffer } from 'buffer'
 import './style.css'
 import App from './App.jsx'
+// import { Buffer } from 'buffer';
 
-// globalThis.Buffer = Buffer
+// Ensure Buffer is globally available
+window.Buffer = Buffer;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
